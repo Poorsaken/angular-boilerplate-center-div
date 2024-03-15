b81fa3a2953310abd9f72ad5d82d468996fd2d14
Poorsaken <chiyokogaming02@gmail.com>
2024-03-15 23:05:33 +0800

    reactive forms march 2

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+
+  todoItem = new FormControl("");
+  todoList: string[] = []
+
+  pushItemToList() {
+
+
+    if (this.todoItem.valid) {
+
+      this.todoList.push(this.todoItem.value as string);
+      this.todoItem.setValue("")
+    }
+    // this.todoList.push(this.todoItem.value as string)
+
+    // this.todoItem.setValue("")
+  }
+}
